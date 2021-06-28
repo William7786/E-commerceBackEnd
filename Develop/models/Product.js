83227fe207ffc438ec293c97c4812a7a25a64627
Tcomp <williamt7786@gmail.com>
2021-06-28 18:08:23 -0400

    this is for the start of product.js

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,6 +9,21 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    id:{
+
+    },
+    product_name:{
+
+    },
+    price:{
+
+    },
+    stock:{
+      
+    },
+    category_id:{
+
+    }
     // define columns
   },
   {
